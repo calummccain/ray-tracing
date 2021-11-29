ad78b44d5c6a45c4762415795cb1c0ae70f67884
Calum McCain <cmccain97@gmail.com>
2021-11-29 12:25:55 +0000

    tighter constants

--- resources/constants.go
+++ resources/constants.go
@@ -2,12 +2,12 @@ package resources
 
 const (
 	NumberOfIterations         = 200
-	RayMarchEps                = 0.00000001
-	CalcNormalEps              = 0.00000001
-	GenerateFacesHyperbolicEps = 0.00000001
-	GenerateFacesSphericalEps  = 0.00000001
-	RayTraceShift              = 0.00000001
-	FactorCutoff               = 0.00001
+	RayMarchEps                = 0.000000001
+	CalcNormalEps              = 0.000000001
+	GenerateFacesHyperbolicEps = 0.000000001
+	GenerateFacesSphericalEps  = 0.000000001
+	RayTraceShift              = 0.00000005
+	FactorCutoff               = 0.000001
 	MaxRayMarchDepth           = 2500
 	SmaxK                      = 0.01
 	SminK                      = 0.01
